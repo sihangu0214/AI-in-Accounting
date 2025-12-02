27658afbad5e346aa62be7dd20d3d4a7848d57e7
sihangu0214 <sihangu@outlook.com>
2025-12-02 03:09:15 +0000

    1

--- app.py
+++ app.py
@@ -1,19 +1,29 @@
-from flask import Flask,render_template,request
+# flask
 
-app=Flask(__name__)
+from flask import Flask, render_template, request
+import joblib
 
-@app.route("/",methods=["GET","POST"])
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    r=request.form.get("q")
+    r = request.form.get("q")
     return(render_template("index.html"))
 
-@app.route("/main",methods=["GET","POST"])
+@app.route("/main", methods=["GET", "POST"])
 def main():
     return(render_template("main.html"))
 
-@app.route("/dbs",methods=["GET","POST"])
+@app.route("/dbs", methods=["GET", "POST"])
 def dbs():
     return(render_template("dbs.html"))
 
-if __name__=="__main__":
-    app.run()
+@app.route("/DbsPrediction", methods=["GET", "POST"])
+def DbsPrediction():
+    q = float(request.form.get("q"))
+    model = joblib.load("dbs.jl")
+    r = model.predict([[q]])
+    return(render_template("DbsPrediction.html", r=r[0][0]))
+
+if __name__ == "__main__":
+    app.run(port=1234)
